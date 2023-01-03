41ea280870d1010147e5962c2909bbf64ddc3aa1
Hugo Silva <jhssilva96@gmail.com>
2023-01-03 19:09:47 +0100

    Setting timer for points
    Setting timer for points in the end.

--- Wordle.gd
+++ Wordle.gd
@@ -6,6 +6,8 @@ var modes_scene
 var game_scene
 var languages = ["pt", "en"]
 var current_language
+var score = 0
+var score_points = [-8, 32, 16, 8 , 4, 2, 1]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,6 +22,7 @@ func set_initial_variables():
 	modes_control_scene = $Modes/Control
 	modes_scene = $Modes
 	game_scene = $Game
+	score = 0
 
 func hide_all_scenes():
 	initial_screen_scene.hide()
@@ -47,3 +50,21 @@ func game_start():
 	game_scene.set_current_language(current_language)
 	game_scene.show()
 	game_scene.start()
+
+func _on_Game_won_game():
+	var level = game_scene.current_row_option
+	var points_won = score_points[level]
+	score += points_won
+	$Timer_end_game.start()
+	# Display victory screen with current points and points won
+
+func _on_Game_lost_game():
+	var points_lost = score_points[0]
+	score += points_lost
+	$Timer_end_game.start()
+	# Display lost screen with current points and points won
+	
+
+func _on_Timer_end_game_timeout():
+	hide_all_scenes()
+	initial_screen_scene.show() #Change this
